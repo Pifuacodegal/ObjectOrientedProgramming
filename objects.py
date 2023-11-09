b617d0fd4c260c66cbe4ed874c53ec1a58af2e27
Pifuacodegal <143063013+Pifuacodegal@users.noreply.github.com>
2023-11-09 12:24:03 +0300

    Assignment 3 Object Oriented Programming
    REG NO: 2023/DCSE/0095/SS

---
+++ objects.py
@@ -0,0 +1,34 @@
+# Create a user class with properties ie name, age, location:
+class User:
+     def __init__(User, name, age, location):
+          User.name=name
+          User.age=age
+          User.location=location
+name=input("Enter your name:")
+age=input("Enter your age:")
+location=("Enter your location:")
+print(User)
+
+    
+#Create a new instance of the  user class (a new object)
+
+#Access the user's name and age
+class User:
+     def __init__(User, name, age):
+          User.name=name
+          User.age=age
+name=input("Enter your name:")
+age=input("Enter your age:")
+print(name)
+print(age)
+
+
+#Create a function for the User class that prints a user's location
+#Print the User's location using this function
+class User:
+     def __init__(User, location):
+          User.location=location
+          
+location=input("Enter your location:")
+print(location)
+
